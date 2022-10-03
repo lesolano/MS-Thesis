cd0ab2f265fe4a5cc024485a78a7a5535d36f65e
lesolano <72961771+lesolano@users.noreply.github.com>
2022-10-03 00:03:18 -0700

    Update DESeq2_DGE_Analysis.R

--- DESeq2_DGE_Analysis.R
+++ DESeq2_DGE_Analysis.R
@@ -3,6 +3,8 @@
 #load libraries####
 library(DESeq2)
 library(tidyverse)
+library('biomaRt')
+
 
 #import ENSG IDs and raw read counts####
 Exp1_countsimport<-read.csv(file = "rawcounts2021.csv", header = TRUE)
@@ -51,10 +53,55 @@ HeLa0RvsHeLaControl<-results(dds, contrast=c("Condition_NBE","HeLa_0R","HeLa_Con
 HeLa8RvsHeLaControl <- results(dds, contrast=c("Condition_NBE","HeLa_8R","HeLa_Control"), alpha = 0.05)
 HeLa8RvsHeLa0R <- results(dds, contrast=c("Condition_NBE","HeLa_8R","HeLa_0R"), alpha = 0.05)
 
-#Uncomment lines below to export DEG results to a .CSV
-#write.csv(as.data.frame(HeLa0RvsHeLaControl), file="HeLa0RvsHeLaControl_NovogeneRawCounts.csv")
-#write.csv(as.data.frame(HeLa8RvsHeLaControl), file="HeLa8RvsHeLaControl_NovogeneRawCounts.csv")
-#write.csv(as.data.frame(HeLa8RvsHeLa0R), file="HeLa8RvsHeLa0R_NovogeneRawCounts.csv")
+#Convert ENSG IDs to HGNC IDs for .csv export####
+#Define "biomart" to be used
+mart<- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+
+#Capture Gene names from DESeq2 result objects
+HeLa0RvsHeLaControl_ENSG<-row.names(HeLa0RvsHeLaControl)
+HeLa8RvsHeLaControl_ENSG<-row.names(HeLa8RvsHeLaControl)
+HeLa8RvsHeLa0R_ENSG<-row.names(HeLa8RvsHeLa0R)
+
+#Create lookup table/retrieve desired annotations.
+HeLa0RvsHeLaControl_ENSGlookup <- getBM(
+  mart = mart,
+  attributes = c('ensembl_gene_id',
+                 'gene_biotype','hgnc_symbol'),
+  filter = 'ensembl_gene_id',
+  values = HeLa0RvsHeLaControl_ENSG,
+  uniqueRows = TRUE)
+
+HeLa8RvsHeLaControl_ENSGlookup <- getBM(
+  mart = mart,
+  attributes = c('ensembl_gene_id',
+                 'gene_biotype','hgnc_symbol'),
+  filter = 'ensembl_gene_id',
+  values = HeLa8RvsHeLaControl_ENSG,
+  uniqueRows = TRUE)
+
+HeLa8RvsHeLa0R_ENSGlookup <- getBM(
+  mart = mart,
+  attributes = c('ensembl_gene_id',
+                 'gene_biotype','hgnc_symbol'),
+  filter = 'ensembl_gene_id',
+  values = HeLa8RvsHeLa0R_ENSG,
+  uniqueRows = TRUE)
+
+#prep a column with ENSG IDs from DESeq2 results object and full join merge with lookup table by "ensembl_gene_id"
+HeLa0RvsHeLaControl_DEG_colprep<-rownames_to_column(as.data.frame(HeLa0RvsHeLaControl), "ensembl_gene_id")
+HeLa0RvsHeLaControl_DEG_Results<-full_join(HeLa0RvsHeLaControl_DEG_colprep,HeLa0RvsHeLaControl_ENSGlookup,by ="ensembl_gene_id", keep=FALSE)
+
+HeLa8RvsHeLaControl_DEG_colprep<-rownames_to_column(as.data.frame(HeLa8RvsHeLaControl), "ensembl_gene_id")
+HeLa8RvsHeLaControl_DEG_Results<-full_join(HeLa8RvsHeLaControl_DEG_colprep,HeLa8RvsHeLaControl_ENSGlookup,by ="ensembl_gene_id", keep=FALSE)
+
+HeLa8RvsHeLa0R_DEG_colprep<-rownames_to_column(as.data.frame(HeLa8RvsHeLa0R), "ensembl_gene_id")
+HeLa8RvsHeLa0R_DEG_Results<-full_join(HeLa8RvsHeLa0R_DEG_colprep,HeLa8RvsHeLa0R_ENSGlookup,by ="ensembl_gene_id", keep=FALSE)
+
+
+#Uncomment lines below to export as .CSV
+#write.csv(as.data.frame(HeLa0RvsHeLaControl_DEG_Results), file="HeLa0RvsHeLaControl_DEG_Results.csv")
+#write.csv(as.data.frame(HeLa8RvsHeLaControl_DEG_Results), file="HeLa8RvsHeLaControl_DEG_Results.csv")
+#write.csv(as.data.frame(HeLa8RvsHeLa0R_DEG_Results), file="HeLa8RvsHeLa0R_DEG_Results.csv")
 
 #examples for how to order results by pvalue 
 #Create p-value ordered results
