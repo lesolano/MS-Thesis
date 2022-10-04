4269f715733223c47db4e4b56e1353750684eb40
lesolano <72961771+lesolano@users.noreply.github.com>
2022-10-04 11:21:43 -0700

    Add files via upload

---
+++ category.R
@@ -0,0 +1,10 @@
+boxplot.category <- boxplot.category[-c(1), ]
+colnames(boxplot.category) <- c('37', '42T0', '42T8')
+rownames(boxplot.category) <- c('1','2','3','4','5','6')
+library(ggplot2)
+library(reshape2)
+boxplot.category_long <- melt(boxplot.category, id=0)
+View(boxplot.category_long)
+ggplot(boxplot.category_long, aes(x=variable, y=value)) + geom_boxplot()
+
+ggplot(boxplot.formatting, aes(x=Lipid, y=MassSpecValue, color=Treatment)) + geom_boxplot() + geom_jitter(shape = 16, position=position_jitter(0.2))
