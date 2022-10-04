e69fb97a5240bd948197079f3b9d20c00fc3ef9a
lesolano <72961771+lesolano@users.noreply.github.com>
2022-10-04 11:24:14 -0700

    Update and rename category.R to Raw_Lipid_Abundance_Boxplots.R

--- Raw_Lipid_Abundance_Boxplots.R
+++ Raw_Lipid_Abundance_Boxplots.R
@@ -1,8 +1,13 @@
+#Original script by Alison Cher and adapted by Luis Solano
+#load libraries
+library(ggplot2)
+library(reshape2)
+
+#Define boxplot parameters
 boxplot.category <- boxplot.category[-c(1), ]
 colnames(boxplot.category) <- c('37', '42T0', '42T8')
 rownames(boxplot.category) <- c('1','2','3','4','5','6')
-library(ggplot2)
-library(reshape2)
+
 boxplot.category_long <- melt(boxplot.category, id=0)
 View(boxplot.category_long)
 ggplot(boxplot.category_long, aes(x=variable, y=value)) + geom_boxplot()
