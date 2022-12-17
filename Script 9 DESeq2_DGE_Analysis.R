ea84c1dfa30818bb415397d1cc511f5f1dd66194
lesolano <72961771+lesolano@users.noreply.github.com>
2022-12-16 19:42:49 -0800

    Update Script 9 DESeq2_DGE_Analysis.R

--- Script 9 DESeq2_DGE_Analysis.R
+++ Script 9 DESeq2_DGE_Analysis.R
@@ -8,28 +8,20 @@ library(tidyverse)
 library('biomaRt')
 
 
-#import ENSG IDs and raw read counts####
+#import ENSG IDs and read counts Make a table with relevant annotations####
 Exp1_countsimport<-read.csv(file = "rawcounts2021.csv", header = TRUE)
-Exp2_countsimport<-read.csv(file = "rawcounts2022.csv", header = TRUE)
 
-#Create a single dataframe from the raw counts data with shared ENSG ID column
-Exp1_2_countsimport<-full_join(Exp1_countsimport,Exp2_countsimport,by ="gene_id", keep=FALSE)
-
-#Define cts and coldata objects with appropriate annotations 
-RawReadCounts<-Exp1_2_countsimport
+#Prepare cts and coldata objects
+RawReadCounts<-Exp1_countsimport
 rownames(RawReadCounts)<-c(RawReadCounts$gene_id)
 cts<-RawReadCounts[,-1]
-coldata<-read.csv(file = 'coldata220924.csv',header = TRUE)
+coldata<-read.csv(file = 'coldata2021.csv',header = TRUE)
 row.names(coldata)<-c(coldata$X)
 all(rownames(coldata) %in% colnames(cts))
-#If this line returns FALSE, uncomment the indicated lines of code below
 all(rownames(coldata) == colnames(cts))
 
-#This ensures coldata rownames match the column names of the cts object exactly
 cts <- cts[, rownames(coldata)]
 all(rownames(coldata) == colnames(cts))
-
-#Create dds object and add metadata
 dds <- DESeqDataSetFromMatrix(countData = cts,
                               colData = coldata,
                               design = ~ Condition_NBE)
@@ -38,22 +30,25 @@ featureData <- data.frame(gene=rownames(cts))
 mcols(dds) <- DataFrame(mcols(dds), featureData)
 
 #factor condition and set levels
-dds$Condition_NBE <- factor(dds$Condition_NBE, levels = c("HeLa_0R","HeLa_8R",
-                                                          "HeLa_Control","HEK_Control",
+dds$Condition_NBE <- factor(dds$Condition_NBE, levels = c("HeLa_Control","HeLa_0R","HeLa_8R",
+                                                          "HEK_Control",
                                                           "HEK_0R","HEK_8R",
                                                           "Hep_Control","Hep_0R",
                                                           "Hep_8R"))
 
-#apply prefilter to dds object of readcounts>9
-keep <- rowSums(counts(dds)) >= 10
+#Uncomment to apply prefilter to dds object of readcounts>9
+keep <- rowSums(counts(dds)) >= 250
 dds <- dds[keep,]
-
-#RunDeseq2 by contrast, store results to descriptively named results objects
-#Use contrast to designate comparisons of interest.  
+#Run DESEQ function on dds object, and save to the same object
 dds <- DESeq(dds)
+
 HeLa0RvsHeLaControl<-results(dds, contrast=c("Condition_NBE","HeLa_0R","HeLa_Control"), alpha = 0.05)
 HeLa8RvsHeLaControl <- results(dds, contrast=c("Condition_NBE","HeLa_8R","HeLa_Control"), alpha = 0.05)
 HeLa8RvsHeLa0R <- results(dds, contrast=c("Condition_NBE","HeLa_8R","HeLa_0R"), alpha = 0.05)
+
+summary(HeLa0RvsHeLaControl)
+summary(HeLa8RvsHeLaControl)
+summary(HeLa8RvsHeLa0R)
 
 #Convert ENSG IDs to HGNC IDs for .csv export####
 #Define "biomart" to be used
